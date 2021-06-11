e199c44a9954613005374a4aebcd6a719e2bcc99
St4rg33k <julienwetzel@gmail.com>
2021-06-11 15:49:04 +0200

    Update FGT-001_Heat_Controller_Summer_mode.lua

--- LUA Scene/FGT-001_Heat_Controller_Summer_mode.lua
+++ LUA Scene/FGT-001_Heat_Controller_Summer_mode.lua
@@ -1,4 +1,13 @@
-print("----------------------------------------------------")
+--[[    L'activation de cette scène va rechercher toutes les zones programmées ainsi que 
+        toutes les vannes associées aux zones et désactive la programmation des zones puis
+        configure les vannes FGT-001 au max (ManufacturerSpecific).
+        Il est nécessaire de désactiver toutes les autres scènes suceptibles de modifier
+        l'état des vannes.
+        Cette scène a été édité par Julien Wetzel
+        Le code source et les mises à jours se trouvent au lien suivant :
+        https://github.com/st4rg33k/Fibaro-HC3 
+]]
+
 local climateZones = api.get("/panels/climate?detailed=true")
 
 for k,zone in pairs(climateZones) do
@@ -6,14 +15,14 @@ for k,zone in pairs(climateZones) do
 
   if zone.active == true then
     api.put("/panels/climate/"..k, {active = {"false"}})
-    print("Désactivation de la zone "..zone.name)
+    fibaro.debug("CLIMAT","Désactivation de la zone "..zone.name)
   end
 
   for i,id in pairs(devicesList) do
 
     if fibaro.getValue(id, "thermostatMode") ~= "ManufacturerSpecific" then
       api.post("/devices/"..id.."/action/setThermostatMode", {args = {"ManufacturerSpecific"}})
-      print("Mode été pour la vanne id: "..id)
+      fibaro.debug("CLIMAT","Mode été pour la vanne id: "..id)
     end
   end
 end
