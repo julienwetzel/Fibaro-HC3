e3ebca44518ab0536cd3ccaf197eeae07db4d043
Julien Wetzel <julienwetzel@gmail.com>
2021-07-26 14:50:26 +0200

    Update Utilities.lua

--- QuickApp/Swisscom Internet Box/Utilities.lua
+++ QuickApp/Swisscom Internet Box/Utilities.lua
@@ -1,3 +1,19 @@
+--[[
+   Copyright [2021] [Julien Wetzel]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+]]
+
 function isEmpty(s) return s == nil or s == "" end
 function cmd(t) QuickApp:trace("CMDEvent: " ..t) end
 
@@ -18,4 +34,11 @@ function TableConcat(t1,t2)
         t1[#t1+1] = t2[i]
     end]]
     return t1
+end
+function toboolean(b)
+    if b == "true" then return true
+    elseif b == "false" then return false
+    elseif b == true or b == false then return b
+    else QuickApp:error("toboolean not work with " .. b) return
+    end
 end
